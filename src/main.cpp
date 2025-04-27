f270ea7245c659cd76cb0a853e06c2e5b0f0b62b
Whittley-chan <reinee066@gmail.com>
2025-04-27 19:05:14 +0700

    1

--- src/main.cpp
+++ src/main.cpp
@@ -4,21 +4,27 @@
 #include "SDL3/SDL_main.h"
 
 #include "graphics.h"
+#include "main.h"
 
-typedef struct 
-        SDL_AppState
+#include <cstdlib>
+#include <iostream>
+
+std::ostream& operator<<(std::ostream& os, const GameState gs)
 	{
-	    int dummy;
-	}   SDL_AppState;
+	    os<< gs;
+	    return os;
+	}
+
 
 void SDL_AppQuit(void* appstate, SDL_AppResult result) 
     {
-
+        SDL_free(appstate);
     }
 
 
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) 
     {
+	    Appstate* as = (Appstate*)appstate;
         if (event->type == SDL_EVENT_QUIT) 
             {
                 return SDL_APP_SUCCESS;
@@ -29,13 +35,29 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 
 SDL_AppResult SDL_AppIterate(void* appstate) // Main game loop - Everything runs in realtime
     {   
+	    Appstate* as = (Appstate*)appstate;
         Render();
         return SDL_APP_CONTINUE;
     }
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char** argv) 
     {
-        Init();
+	    Appstate* as = (Appstate*)SDL_calloc(1, sizeof(Appstate));
+        
+
+
+		if (!as)
+			{
+			    return SDL_APP_FAILURE;
+			}
+		else
+			{
+			    *appstate = as;
+			}
+
+        Init(as);
+        
+
 
         return SDL_APP_CONTINUE;
     }
