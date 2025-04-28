f52b0bb94db0385313ab2daecf56d70568ac8983
Whittley-chan <reinee066@gmail.com>
2025-04-28 17:08:17 +0700

    splited code into multiple files

--- src/main.cpp
+++ src/main.cpp
@@ -3,17 +3,11 @@
 #include "SDL3/SDL.h"
 #include "SDL3/SDL_main.h"
 
-#include "graphics.h"
 #include "main.h"
 
-#include <cstdlib>
-#include <iostream>
 
-std::ostream& operator<<(std::ostream& os, const GameState gs)
-	{
-	    os<< gs;
-	    return os;
-	}
+
+
 
 
 void SDL_AppQuit(void* appstate, SDL_AppResult result) 
@@ -25,27 +19,63 @@ void SDL_AppQuit(void* appstate, SDL_AppResult result)
 SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event) 
     {
 	    Appstate* as = (Appstate*)appstate;
-        if (event->type == SDL_EVENT_QUIT) 
+        
+        switch(event->type)
             {
-                return SDL_APP_SUCCESS;
+                case SDL_EVENT_QUIT:
+                    {
+                        return SDL_APP_SUCCESS;
+                        break;
+                    }
+                case SDL_EVENT_MOUSE_BUTTON_DOWN:
+					{
+					    if (event->button.button == SDL_BUTTON_LEFT)
+						    {
+							switch (as->Context)
+								{
+									case MAIN_MENU:
+										{
+											MainMenuMouseHandler(as, event);
+											break;
+										}
+									case IN_GAME:
+										{
+											SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "DEBUG", "In Game Clicked", as->window);
+											break;
+										}
+									case PAUSE_MENU:
+										{
+											SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "DEBUG", "Pause Menu Clicked", as->window);
+											break;
+										}
+									default:
+										{
+											break;
+										}
+								}
+						    }
+					    break;
+					}
+                default:
+                    {
+                        break;
+                    }
             }
         return SDL_APP_CONTINUE;
     }
 
 
-SDL_AppResult SDL_AppIterate(void* appstate) // Main game loop - Everything runs in realtime
-    {   
+SDL_AppResult SDL_AppIterate(void* appstate)
+	{
 	    Appstate* as = (Appstate*)appstate;
-        Render();
+        Render(as);
         return SDL_APP_CONTINUE;
     }
 
-SDL_AppResult SDL_AppInit(void **appstate, int argc, char** argv) 
+SDL_AppResult SDL_AppInit(void **appstate, int argc, char** argv)
     {
 	    Appstate* as = (Appstate*)SDL_calloc(1, sizeof(Appstate));
         
-
-
 		if (!as)
 			{
 			    return SDL_APP_FAILURE;
@@ -57,7 +87,6 @@ SDL_AppResult SDL_AppInit(void **appstate, int argc, char** argv)
 
         Init(as);
         
-
 
         return SDL_APP_CONTINUE;
     }
