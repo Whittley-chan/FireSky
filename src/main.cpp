d508fa7bc6329014177651dd1e8c5c8138474436
Whittley-chan <reinee066@gmail.com>
2025-04-29 19:38:13 +0700

    Optimized rendering process, added Ingame rendering

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,6 @@
 #define SDL_MAIN_USE_CALLBACKS
 
+#include <vector>
 #include "SDL3/SDL.h"
 #include "SDL3/SDL_main.h"
 
@@ -40,10 +41,10 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 										}
 									case IN_GAME:
 										{
-											SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "DEBUG", "In Game Clicked", as->window);
+											IngameMouseHandler(as, event);
 											break;
 										}
-									case PAUSE_MENU:
+									case PAUSED:
 										{
 											SDL_ShowSimpleMessageBox(SDL_MESSAGEBOX_INFORMATION, "DEBUG", "Pause Menu Clicked", as->window);
 											break;
@@ -68,14 +69,21 @@ SDL_AppResult SDL_AppEvent(void *appstate, SDL_Event *event)
 SDL_AppResult SDL_AppIterate(void* appstate)
 	{
 	    Appstate* as = (Appstate*)appstate;
+		if (as->Context == QUIT)
+			{
+				return SDL_APP_SUCCESS;
+			}
         Render(as);
+		SDL_RenderPresent(as->renderer);
+		SDL_Delay(16);
         return SDL_APP_CONTINUE;
     }
 
 SDL_AppResult SDL_AppInit(void **appstate, int argc, char** argv)
     {
 	    Appstate* as = (Appstate*)SDL_calloc(1, sizeof(Appstate));
-        
+
+
 		if (!as)
 			{
 			    return SDL_APP_FAILURE;
